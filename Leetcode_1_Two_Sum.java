c62a69275ca47bbbe4c296a35d2efcbdc5b237ac
George <leo1486206941@gmail.com>
2021-05-04 22:49:53 +1000

    optimised solutions added

--- Leetcode_1_Two_Sum.java
+++ Leetcode_1_Two_Sum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetcode_1_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i < nums.length; i++) {
@@ -9,5 +12,33 @@ public class Leetcode_1_Two_Sum {
         }
 
         return new int[2];
+    }
+
+    //use hashmap to low down the complexity from O(n^2) to O(n)
+    public int[] twoSum_Optimum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            int sub=target-nums[i];
+            if(map.containsKey(sub)&&map.get(sub)!=i){
+                return new int[]{i,map.get(sub)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    //combine the hashmap gen and the calculation
+    public int[] twoSum_Fastest(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int sub=target-nums[i];
+            if(map.containsKey(sub)){
+                return new int[]{i,map.get(sub)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
