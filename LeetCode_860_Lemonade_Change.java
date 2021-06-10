9d983205ed0202afa8b21904c4c834b86208d7ef
George Liu <leo1486206941@gmail.com>
2021-06-10 21:30:52 +1000

    add leetcode 860 initial solutions

---
+++ LeetCode_860_Lemonade_Change.java
@@ -0,0 +1,32 @@
+public class LeetCode_860_Lemonade_Change {
+    public boolean lemonadeChange(int[] bills) {
+        int fiveDollersChangeLeft = 0;
+        int tenDollersChangeLeft = 0;
+
+        for (int i = 0; i < bills.length; i ++) {
+            if (bills[i] == 5) {
+                fiveDollersChangeLeft++;
+            }
+
+            if (bills[i] == 10) {
+                fiveDollersChangeLeft -= bills[i]/5 - 1;
+                tenDollersChangeLeft ++;
+            }
+
+            if (bills[i] == 20) {
+                if (tenDollersChangeLeft > 0) {
+                    tenDollersChangeLeft--;
+                    fiveDollersChangeLeft--;
+                } else {
+                    fiveDollersChangeLeft -= 3;
+                }
+            }
+
+            if (fiveDollersChangeLeft < 0 || tenDollersChangeLeft < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
