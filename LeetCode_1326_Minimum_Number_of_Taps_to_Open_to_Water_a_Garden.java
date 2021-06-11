1faa801a8edc5a65672781a185e235eee90992c5
George Liu <leo1486206941@gmail.com>
2021-06-11 14:32:55 +1000

    add leetcode 1326

---
+++ LeetCode_1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden.java
@@ -0,0 +1,6 @@
+//This is a hard problem I leave it for now
+public class LeetCode_1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden {
+    public int minTaps(int n, int[] ranges) {
+        return -1;
+    }
+}
