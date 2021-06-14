e522f099b3f36fce9ac697d8afa06e44d537d307
George Liu <leo1486206941@gmail.com>
2021-06-15 01:35:13 +1000

    add initial solution for leetcode 53

---
+++ LeetCode_53_Maximum_Subarray.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LeetCode_53_Maximum_Subarray {
+    public int maxSubArray(int[] nums) {
+        int globalSum = Integer.MIN_VALUE;
+        int localSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            localSum = nums[i] + localSum;
+            if (localSum < nums[i]) {
+                localSum = nums[i];
+            }
+
+            globalSum = Math.max(globalSum, localSum);
+        }
+
+        return globalSum;
+    }
+}
