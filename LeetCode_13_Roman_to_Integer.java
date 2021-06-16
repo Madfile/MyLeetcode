ab1c54b62bac1b8dad7e15eaa74ab5876fff57d8
George Liu <leo1486206941@gmail.com>
2021-06-16 16:41:00 +1000

    add initial solution and optimal solution for leetcode 13

---
+++ LeetCode_13_Roman_to_Integer.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_13_Roman_to_Integer {
+    public int romanToInt(String s) {
+        Map<Character, Integer> valueMap = new HashMap<Character, Integer>();
+
+        valueMap.put('I', 1);
+        valueMap.put('V', 5);
+        valueMap.put('X', 10);
+        valueMap.put('L', 50);
+        valueMap.put('C', 100);
+        valueMap.put('D', 500);
+        valueMap.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            int currentValue = valueMap.get(s.charAt(i));
+            int previousValue = valueMap.get(s.charAt(i - 1));
+            if (currentValue > previousValue) {
+                result -= previousValue;
+            } else {
+                result += previousValue;
+            }
+        }
+
+        result += valueMap.get(s.charAt(s.length() - 1));
+
+        return result;
+    }
+}
