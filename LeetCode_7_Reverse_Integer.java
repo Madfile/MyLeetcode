82db7998156fd3a5ce503084ab1f3c7acfdcc11d
George Liu <leo1486206941@gmail.com>
2021-06-16 15:32:02 +1000

    add optimal solution for leetcode 7

--- LeetCode_7_Reverse_Integer.java
+++ LeetCode_7_Reverse_Integer.java
@@ -25,4 +25,16 @@ public class LeetCode_7_Reverse_Integer {
 
         return positive ? (int)newValue : (int)newValue * (-1);
     }
+
+    //optimal solution
+//    public int reverse(int x) {
+//        long rev = 0;
+//        while (x != 0) {
+//            int pop = x % 10;
+//            x /= 10;
+//            rev = rev * 10 + pop;
+//        }
+//        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE ) return 0;
+//        return (int)rev;
+//    }
 }
