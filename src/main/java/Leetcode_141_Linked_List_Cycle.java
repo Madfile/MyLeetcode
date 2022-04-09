411f8f8ee7be9496d051cf829bcdf72a8fda94df
George Liu <leo1486206941@gmail.com>
2022-04-09 19:28:32 +1000

    add initial solution and optimal solution for leetcode 141

---
+++ src/main/java/Leetcode_141_Linked_List_Cycle.java
@@ -0,0 +1,32 @@
+package src.main.java;
+
+public class Leetcode_141_Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            if (slow == fast.next || slow == fast.next.next) {
+                return true;
+            } else {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+        }
+
+        return false;
+    }
+    
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+}
