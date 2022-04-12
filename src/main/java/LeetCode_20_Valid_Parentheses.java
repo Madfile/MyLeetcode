cd80ef312e89b9d01d329b0ffc14ccd8361b58ea
George Liu <leo1486206941@gmail.com>
2022-04-12 20:20:43 +1000

    add initial solution and optimal solution for leetcode 20

--- src/main/java/LeetCode_20_Valid_Parentheses.java
+++ src/main/java/LeetCode_20_Valid_Parentheses.java
@@ -1,44 +1,65 @@
 package src.main.java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class LeetCode_20_Valid_Parentheses {
     //initial commit
     public boolean isValid(String s) {
-        ArrayList<String> leftBracketsPool = new ArrayList<String>();
-        ArrayList<String> leftBrakcets = new ArrayList<String>(Arrays.asList("(", "[", "{"));
-        ArrayList<String> rightBrakcets = new ArrayList<String>(Arrays.asList(")", "]", "}"));
+//        ArrayList<String> leftBracketsPool = new ArrayList<String>();
+//        ArrayList<String> leftBrakcets = new ArrayList<String>(Arrays.asList("(", "[", "{"));
+//        ArrayList<String> rightBrakcets = new ArrayList<String>(Arrays.asList(")", "]", "}"));
+//
+//        for(int i = 0; i < s.length(); i++) {
+//            String charToTest = String.valueOf(s.charAt(i));
+//            if(leftBrakcets.contains(charToTest)) {
+//                leftBracketsPool.add(charToTest);
+//            }
+//
+//            if (rightBrakcets.contains(charToTest)) {
+//                if (leftBracketsPool.size() == 0 || !leftBracketsPool.get(leftBracketsPool.size() - 1).equals(mapBracket(charToTest))) {
+//                    return false;
+//                } else {
+//                    leftBracketsPool.remove(leftBracketsPool.size() - 1);
+//                }
+//            }
+//        }
+//
+//        if(leftBracketsPool.size() == 0) {
+//            return true;
+//        } else return false;
+//    }
+//
+//    public String mapBracket(String bracket) {
+//        if(bracket.equals(")")) {
+//            return "(";
+//        } else if (bracket.equals("]")) {
+//            return "[";
+//        } else if (bracket.equals("}")) {
+//            return "{";
+//        } else return null;
+//    }
 
-        for(int i = 0; i < s.length(); i++) {
-            String charToTest = String.valueOf(s.charAt(i));
-            if(leftBrakcets.contains(charToTest)) {
-                leftBracketsPool.add(charToTest);
-            }
+    Map<Character, Character> charMap = new HashMap<>();
+        charMap.put(')','(');
+        charMap.put(']','[');
+        charMap.put('}','{');
 
-            if (rightBrakcets.contains(charToTest)) {
-                if (leftBracketsPool.size() == 0 || !leftBracketsPool.get(leftBracketsPool.size() - 1).equals(mapBracket(charToTest))) {
-                    return false;
-                } else {
-                    leftBracketsPool.remove(leftBracketsPool.size() - 1);
-                }
-            }
+    Deque<Character> stack = new ArrayDeque();
+        for (char c: s.toCharArray()) {
+        if (stack.size() == 0) {
+            stack.push(c);
+        } else if (charMap.get(c) != null && stack.peek() == charMap.get(c)) {
+            stack.poll();
+        } else {
+            stack.push(c);
         }
-
-        if(leftBracketsPool.size() == 0) {
-            return true;
-        } else return false;
     }
 
-    public String mapBracket(String bracket) {
-        if(bracket.equals(")")) {
-            return "(";
-        } else if (bracket.equals("]")) {
-            return "[";
-        } else if (bracket.equals("}")) {
-            return "{";
-        } else return null;
+        if (stack.size() != 0) {
+        return false;
     }
+
+        return true;
 }
 
 //optimal solution (use Stack)
