b1983bf89d94a712932a0d7626551334386c2bf4
georgeliu <leo1486206941@gmail.com>
2022-05-24 22:21:47 +1000

    Optimised solution to 1249

---
+++ src/main/java/Leetcode_1249_Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Leetcode_1249_Minimum_Remove_to_Make_Valid_Parentheses {
+    class Solution {
+        public String minRemoveToMakeValid(String s) {
+            List<Character> result = new ArrayList<>();
+            Map<Character, Character> brackets = new HashMap<Character, Character>();
+            brackets.put('(' , ')');
+
+            Deque<Character> stack = new ArrayDeque<>();
+            for (char c: s.toCharArray()) {
+                if (c != '(' && c != ')') {
+                    result.add(c);
+                } else if (c == '(') {
+                    stack.push('(');
+                    result.add(c);
+                } else if (c == ')') {
+                    if (stack.size() == 0) {
+                        continue;
+                    }
+
+                    //char next = stack.poll();
+                    stack.poll();
+                    result.add(c);
+                }
+            }
+
+            int left = stack.size();
+            //remove the last left number of '('
+            int i = result.size() - 1;
+            while (i >= 0) {
+                if (result.get(i) == '(' && left > 0) {
+                    result.remove(i);
+                    left--;
+                }
+
+                i--;
+            }
+
+
+            StringBuilder resultString = new StringBuilder();
+            for(char c: result) {
+                resultString.append(c);
+            }
+
+            return resultString.toString();
+        }
+    }
+}
